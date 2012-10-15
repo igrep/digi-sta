dafbb60c4e08a97ba24759323daf9f47b52eb1f4
Yuji Yamamoto <whosekiteneverfly@gmail.com>
2012-10-16 06:02:53 +0900

    とりあえずテストクラスのコンストラクタを追加。

---
+++ DigiStaTest/src/info/android/akihabara/cos/sample02/test/StampHistoryActivityTestCase.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package info.android.akihabara.cos.sample02.test;
+import info.android.akihabara.cos.sample02.StampHistoryActivity;
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * @author yu
+ *
+ */
+public class StampHistoryActivityTestCase extends
+		ActivityInstrumentationTestCase2<StampHistoryActivity> {
+
+	public StampHistoryActivityTestCase(String name, Class<StampHistoryActivity> klass) {
+		super(name, klass);
+	}
+
+}
